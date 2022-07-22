252d92e72929b195c69b751ca11dcfbe21eb9c20
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-07-22 14:42:39 +0500

    lecture uploaded

---
+++ WebApplication4/Controllers/AjaxUserController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class AjaxUserController : Controller
+    {
+        // GET: AjaxUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        private readonly User[] userdata =
+        {
+            new User{FirstName ="Arsalan" ,Lastname="Shah",Birthday = new DateTime(2001,4,18) ,role = Role.Normal},
+            new User{FirstName ="Ali" ,Lastname="Shah",Birthday = new DateTime(2001,4,18) ,role = Role.Normal},
+            new User{FirstName ="Affan" ,Lastname="Shah",Birthday = new DateTime(2001,4,18) ,role = Role.Admin},
+            new User{FirstName ="Hassan" ,Lastname="Shah",Birthday = new DateTime(2001,4,18) ,role = Role.Admin},
+            new User{FirstName ="Shahid" ,Lastname="Shah",Birthday = new DateTime(2001,4,18) ,role = Role.Guest},
+
+
+
+        };
+
+
+
+
+        public ActionResult GetUserData(string selectedrole ="All")
+        {
+
+            IEnumerable data = userdata;
+
+            if (selectedrole != "All")
+            {
+
+                var selected = (Role)Enum.Parse(typeof(Role), selectedrole);
+
+                data = userdata.Where(p => p.role == selected);
+
+
+
+
+
+            }
+
+            return View(data);
+        }
+
+
+        public ActionResult GetUser(string selectedrole = "All")
+        {
+            return View((object)selectedrole);
+
+        }
+
+
+        }
+}
---
+++ WebApplication4/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthday{ get; set; }
+        public Role role { get; set; }
+     
+
+        
+    }
+    public enum Role
+    {
+        Admin,Normal, Guest
+
+    }
+
+
+
+}
